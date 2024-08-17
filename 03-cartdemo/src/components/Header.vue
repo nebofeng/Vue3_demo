520858a153f2517c66b4d5054f7c17e8bf8514b0
nebofeng <nebofeng@gmail.com>
2024-08-17 22:32:44 +0800

    [ADD] Header

---
+++ 03-cartdemo/src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+   <div class="main" :style="{ backgroundColor:bgcolor,color:color,fontSize:fsize +'px'}" > {{title}}</div>
+
+</template>
+
+
+<script>
+ export default{
+  name: 'Header',
+     name: '',
+     props:{
+         title:{
+            type:String,
+            default:"demo",
+         },
+         bgcolor: {
+            type: String,
+            default: '#007BFF',
+         },
+         color: {
+            type: String,
+            default: '#ffffff',
+         },
+         fsize:{
+            type:Number,
+            default:12
+         }
+
+
+     }
+
+
+
+ }
+
+
+</script>
+
+<style>
+.main {
+ position: fixed;
+ line-height: 45px;
+ z-index: 9999;
+  top: 0;
+  left: 0;
+  width: 100%;
+
+
+}
+</style>
