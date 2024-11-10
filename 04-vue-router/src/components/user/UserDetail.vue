ba8173ba3ee261ac17f487fb56c99f27a7c983b5
nebofeng <nebofeng@gmail.com>
2024-11-10 12:03:33 +0800

    增加用户详情页面、回退按钮

--- 04-vue-router/src/components/user/UserDetail.vue
+++ 04-vue-router/src/components/user/UserDetail.vue
@@ -1,3 +1,25 @@
 <template>
-    deatils
+    <button  type="button" class="btn" @click="goBack">后退</button>
+    <h4> 用户详情 {{ id}}</h4>
 </template>
+<script>
+
+export default{
+    name:'UserDetail',
+    props:['id'],
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        goBack() {
+            this.$router.go(-1)
+        }
+
+    }
+
+
+}
+
+</script>
